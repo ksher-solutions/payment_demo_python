4ac21aa222d525d57e721e9e07e65a228338dd3b
deemarc <deemarc.br@gmail.com>
2021-06-15 01:41:34 +0700

    change webhook to get method

--- webhook/webhook.py
+++ webhook/webhook.py
@@ -4,10 +4,12 @@ import json
 webhook = Blueprint("webhook", __name__)
 
 
-@webhook.route("/webhook", methods=["POST"])
+@webhook.route("/webhook", methods=["GET"])
 def order():
     data = request.json
+    args = request.args
     current_app.logger.info("================= WEBHOOK =================")
+    current_app.logger.info("raw args data:{}".format(args))
     current_app.logger.info("raw resp data:{}".format(data))
     current_app.logger.info("================= END WEBHOOK =================")
     return json.dumps({"success": True})
